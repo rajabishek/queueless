0c3d384fded687ec6eee2e1740a26c41c5e68586
Raj Abishek <rajabishek@hotmail.com>
2016-01-21 20:02:02 +0530

    Change the model in context of employees

---
+++ app/Employee.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Queueless;
+
+use Gravatar;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Employee extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['fullname', 'email', 'password'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * A user belongs to an organisation.
+     *
+     * @return mixed
+     */
+    public function organisation()
+    {
+        return $this->belongsTo('Queueless\Organisation');
+    }
+
+    /**
+     * Get the user's avatar image.
+     *
+     * @return string
+     */
+    public function getPhotocssAttribute()
+    {
+        if($this->profile && app('filesystem')->disk('local')->exists('public/uploads/profiles' . '/' . $this->profile)) {
+            return url('uploads/profiles/' . $this->profile);
+        }
+
+        return Gravatar::src($this->email, 100);
+    }
+}
