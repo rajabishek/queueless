738cf23d863e5fa2975503f47b2fe9508e401940
Raj Abishek <rajabishek@hotmail.com>
2016-01-21 15:35:57 +0530

    Adds the sericie providers and the global facades

--- config/app.php
+++ config/app.php
@@ -155,6 +155,8 @@ return [
         Queueless\Providers\AuthServiceProvider::class,
         Queueless\Providers\EventServiceProvider::class,
         Queueless\Providers\RouteServiceProvider::class,
+        Queueless\Providers\RepositoryServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
     ],
 
@@ -201,7 +203,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
