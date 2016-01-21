fe2cf96344086f142cd154cf7e053d1d0b8da541
Raj Abishek <rajabishek@hotmail.com>
2016-01-21 20:04:31 +0530

    Adds the route for the web authentication of the app

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,29 @@ Route::group(['namespace' => 'Api\v1','prefix' => 'api/v1'], function ($app)
 		Route::get('token/validate', 'AuthController@validateToken');
 	});
 });
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+Route::group(['middleware' => ['web']], function () {
+    
+    Route::get('/', ['as' => 'pages.home', 'uses' => 'PagesController@getHome']);
+
+    Route::group(['namespace' => 'Auth', 'prefix' => 'auth'], function() {
+    	
+    	Route::get('register', ['as' => 'auth.getRegister', 'uses' => 'AuthController@getRegister']);
+   		Route::post('register', ['as' => 'auth.postRegister', 'uses' => 'AuthController@postRegister']);
+
+   		Route::get('login', ['as' => 'auth.getLogin', 'uses' => 'AuthController@getLogin']);
+   		Route::post('login', ['as' => 'auth.postlogin', 'uses' => 'AuthController@postLogin']);
+    
+    	Route::get('logout', [ 'as' => 'auth.logout', 'uses' => 'AuthController@getLogout']);
+    });
+});
