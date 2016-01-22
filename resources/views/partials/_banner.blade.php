8d8e387545963c59b3d00970f82ad01415c10bbd
Raj Abishek <rajabishek@hotmail.com>
2016-01-22 23:04:03 +0530

    Adds the partial that displays the banner during authentication

---
+++ resources/views/partials/_banner.blade.php
@@ -0,0 +1,5 @@
+<div class="jumbotron">
+    <h1>Queueless</h1>
+    <h5 class="lead">Fully customizable queue management in just minutes.</h5>
+    <a class="btn btn-sm btn-info" role="button" href="/about">Learn more »</a>
+</div>
