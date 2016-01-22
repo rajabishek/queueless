f93c0da3e16350c4e36a59ded62e0283035fcb91
Raj Abishek <rajabishek@hotmail.com>
2016-01-22 23:04:24 +0530

    Adds the view for the home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- Header -->
+@include('partials._header')
+<!-- END Header -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            @include('partials._banner')
+        </div>
+    </div>
+</div>
+@endsection
