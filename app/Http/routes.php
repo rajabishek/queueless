66ee82dd978f6d2a302e535e59c86043fb45f877
Raj Abishek <rajabishek@hotmail.com>
2016-01-24 12:38:42 +0530

    Restructure and organise the route file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,28 +2,6 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::group(['namespace' => 'Api\v1','prefix' => 'api/v1'], function ($app)
-{	
-	Route::group(['namespace' => 'Auth'], function ($app)
-	{
-		Route::post('register','AuthController@postRegister');
-		Route::post('login','AuthController@postLogin');
-		Route::get('user', 'AuthController@getUser');
-		Route::get('token/validate', 'AuthController@validateToken');
-	});
-});
-
-/*
-|--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
@@ -32,18 +10,63 @@ Route::group(['namespace' => 'Api\v1','prefix' => 'api/v1'], function ($app)
 | kernel and includes session state, CSRF protection, and more.
 |
 */
-Route::group(['middleware' => ['web']], function () {
-    
-    Route::get('/', ['as' => 'pages.home', 'uses' => 'PagesController@getHome']);
+
+Route::group(['domain' => env('APP_DOMAIN', 'queueless.com'), 'middleware' => ['web']], function()
+{
+    Route::get('/',['as' => 'pages.home','uses' => 'PagesController@getHome']);
 
     Route::group(['namespace' => 'Auth', 'prefix' => 'auth'], function() {
-    	
-    	Route::get('register', ['as' => 'auth.getRegister', 'uses' => 'AuthController@getRegister']);
-   		Route::post('register', ['as' => 'auth.postRegister', 'uses' => 'AuthController@postRegister']);
-
-   		Route::get('login', ['as' => 'auth.getLogin', 'uses' => 'AuthController@getLogin']);
-   		Route::post('login', ['as' => 'auth.postlogin', 'uses' => 'AuthController@postLogin']);
+        
+        Route::get('register', ['as' => 'auth.getRegister', 'uses' => 'AuthController@getRegister']);
+        Route::post('register', ['as' => 'auth.postRegister', 'uses' => 'AuthController@postRegister']);
+        Route::get('register/verify/{confirmationCode}',['as' => 'auth.register.confirm','uses' => 'AuthController@confirm']);
     
-    	Route::get('logout', [ 'as' => 'auth.logout', 'uses' => 'AuthController@getLogout']);
+        Route::get('verification/resend',['as' => 'auth.verification.getResend','uses' => 'AuthController@getResend']);
+        Route::post('verification/resend',['as' => 'auth.verification.postResend','uses' => 'AuthController@postResend']);
     });
 });
+
+Route::group(['domain' => '{domain}.' . env('APP_DOMAIN','queueless.com'),'middleware' => 'verify-domain'], function()
+{
+    Route::group(['middleware' => ['web']], function(){
+
+        Route::get('/',function($domain){ return redirect()->route('auth.getLogin',$domain); });
+    
+        Route::group(['namespace' => 'Auth', 'prefix' => 'auth'], function()
+        {
+            Route::get('password/email', ['as' => 'password.getEmail', 'uses' => 'PasswordController@getEmail']);
+            Route::post('password/email', ['as' => 'password.postEmail','uses' => 'PasswordController@postEmail']);
+
+            Route::get('password/reset/{token}', ['as' => 'password.getReset','uses' => 'PasswordController@getReset']);
+            Route::post('password/reset', ['as' => 'password.postReset','uses' => 'PasswordController@postReset']);
+
+            Route::get('login', ['as' => 'auth.getLogin', 'uses' => 'AuthController@getLogin']);
+            Route::post('login', ['as' => 'auth.postLogin', 'uses' => 'AuthController@postLogin']);
+        
+            Route::get('logout', [ 'as' => 'auth.logout', 'uses' => 'AuthController@getLogout']);
+        });
+
+        Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'designation:Admin'],function() 
+        {
+            Route::get('users/import',['as' => 'admin.users.getImport','uses' => 'UsersController@getImport']);
+            Route::post('users/import',['as' => 'admin.users.postImport','uses' => 'UsersController@postImport']);
+            Route::get('users/download',['as' => 'admin.users.getDownload','uses' => 'UsersController@getDownload']);
+            Route::post('users/download',['as' => 'admin.users.postDownload','uses' => 'UsersController@postDownload']);
+            Route::resource('users', 'UsersController');
+        });
+    });
+
+    Route::group(['namespace' => 'Api\v1','prefix' => 'api/v1'], function ($app)
+    {   
+        Route::group(['namespace' => 'Auth'], function ($app)
+        {
+            Route::post('register','AuthController@postRegister');
+            Route::post('login','AuthController@postLogin');
+            Route::get('user', 'AuthController@getUser');
+            Route::get('token/validate', 'AuthController@validateToken');
+        });
+    });
+
+});
+
+
