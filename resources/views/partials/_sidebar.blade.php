bc03f4a19ff812d2a80b5c9346cf81d783fba5fe
Raj Abishek <rajabishek@hotmail.com>
2016-01-24 12:45:17 +0530

    Adds the partial for the sidebar

---
+++ resources/views/partials/_sidebar.blade.php
@@ -0,0 +1,18 @@
+<ul class="nav nav-sidebar">
+    <li class="active"><a href="#">Manage Employees</a></li>
+    <li><a href="#">Add Employee</a></li>
+    <li><a href="#">Import List</a></li>
+    <li><a href="#">Export List</a></li>
+</ul>
+<ul class="nav nav-sidebar">
+    <li><a href="">Nav item</a></li>
+    <li><a href="">Nav item again</a></li>
+    <li><a href="">One more nav</a></li>
+    <li><a href="">Another nav item</a></li>
+    <li><a href="">More navigation</a></li>
+</ul>
+<ul class="nav nav-sidebar">
+    <li><a href="">Settings</a></li>
+    <li><a href="">Profile</a></li>
+    <li><a href="">Logout</a></li>
+</ul>
