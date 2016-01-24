b84dd4d819ac8f49432c182797e18e95a1d08b43
Raj Abishek <rajabishek@hotmail.com>
2016-01-24 12:40:16 +0530

    Adds the contract for fetching and persisting employee data

--- app/Repositories/EmployeeRepositoryInterface.php
+++ app/Repositories/EmployeeRepositoryInterface.php
@@ -8,10 +8,46 @@ use Queueless\Organisation;
 interface EmployeeRepositoryInterface
 {
     /**
+     * Find all employees paginated.
+     *
+     * @param  Queueless\Organisation $organisation
+     * @param  int  $perPage
+     * @return Illuminate\Database\Eloquent\Collection|\Queueless\Employee[]
+     */
+    public function findAllPaginatedForOrganisation($organisation, $perPage = 8);
+
+   /**
+     * Find the employee by the given id belonging to the given organisation.
+     *
+     * @param  int  $id
+     * @param  \Queueless\Organisation $organisation
+     * @return \Queueless\Employee
+     */
+    public function findByIdForOrganisation($id, Organisation $organisation);
+
+    /**
+     * Find the admin belonging to the given organisation.
+     *
+     * @param  int  $id
+     * @param  \Queueless\Organisation $organisation
+     * @return \Queueless\Employee
+     */
+    public function getAdminForOrganisation(Organisation $organisation);
+
+    /**
+     * Find the employee by the given email address from the given organisation.
+     *
+     * @param  int  $email
+     * @param  \Queueless\Organisation  $organisation
+     * @return \Queueless\Employee
+     */
+    public function findByEmailForOrganisation($email, Organisation $organisation);
+
+    /**
      * Create a new employee in the database.
      *
      * @param  array $data
-     * @return \Queueless\User
+     * @return \Queueless\Employee
      */
     public function createForOrganisation(array $data, Organisation $organisation);
 }
