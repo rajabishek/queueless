2bfa63c77f1d83ab1fba18df864335b78dce4321
Raj Abishek <rajabishek@hotmail.com>
2016-01-25 21:30:43 +0530

    Use the navigation facade to generate the links in the sidebar:

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,18 +1,6 @@
 <ul class="nav nav-sidebar">
-    <li class="active"><a href="#">Manage Employees</a></li>
-    <li><a href="#">Add Employee</a></li>
-    <li><a href="#">Import List</a></li>
-    <li><a href="#">Export List</a></li>
+    {!! Navigation::make($domain,Request::path()) !!}
 </ul>
 <ul class="nav nav-sidebar">
-    <li><a href="">Nav item</a></li>
-    <li><a href="">Nav item again</a></li>
-    <li><a href="">One more nav</a></li>
-    <li><a href="">Another nav item</a></li>
-    <li><a href="">More navigation</a></li>
-</ul>
-<ul class="nav nav-sidebar">
-    <li><a href="">Settings</a></li>
-    <li><a href="">Profile</a></li>
-    <li><a href="">Logout</a></li>
+    {!! Navigation::make($domain,Request::path(),'adminmenu-bottom') !!}
 </ul>
