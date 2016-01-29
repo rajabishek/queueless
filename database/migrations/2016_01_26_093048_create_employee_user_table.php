ed1551632bc6acd4636dc92eb8964a41060e5d01
Raj Abishek <rajabishek@hotmail.com>
2016-01-30 00:33:49 +0530

    Adds the appointmnets table

---
+++ database/migrations/2016_01_26_093048_create_employee_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_user', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('employee_id')->unsigned();
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->enum('status',['Attending','Done'])->default('Attending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employee_user');
+    }
+}
