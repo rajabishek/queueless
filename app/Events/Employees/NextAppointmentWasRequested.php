95af4560bcd92882d8b8563b61d5da10ddc02b2e
Raj Abishek <rajabishek@hotmail.com>
2016-02-02 23:15:19 +0530

    Fire off an event to notify that an employee request for next apooint ment

---
+++ app/Events/Employees/NextAppointmentWasRequested.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Queueless\Events\Employees;
+
+use Queueless\Organisation;
+use Queueless\Events\Event;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class NextAppointmentWasRequested extends Event implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @var \Queuless\Organisation
+     */
+    public $organisation;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Organisation $organisation)
+    {
+        $this->organisation = $organisation;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return ['organisation.'.$this->organisation->id];
+    }
+}
