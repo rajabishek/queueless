63567faeb50da35c0961af802d71f189631083ff
Raj Abishek <rajabishek@hotmail.com>
2016-02-02 23:13:47 +0530

    Use vue.js for the frontend to manage the websockets using pusher'

--- resources/views/admin/users/queue.blade.php
+++ resources/views/admin/users/queue.blade.php
@@ -6,6 +6,14 @@
 @include('partials._header')
 <!-- END Header -->
 
+@section('scripts')
+@parent
+<script src="//js.pusher.com/3.0/pusher.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.16/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.js"></script>
+<script src="/js/queue.js"></script>
+@endsection
+
 <div class="container">
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
@@ -14,60 +22,51 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         	<div class="row">
         		<h2>Attending Users</h2>
-				@if($attendingUsers->count())
-					<div class="col-sm-10 col-sm-offset-1">
-						<table id="mytable" class="table table-bordred table-striped">
-						    <thead>
-						        <tr>
-						            <th>Employee</th>
-						       		<th>User</th>
-						        </tr>
-						    </thead>
-						    <tbody>
-						        @foreach($attendingUsers as $attendingUser)
-						        	<tr>
-							            <td>{{ $attendingUser->employee->fullname }}</td>
-							            <td>{{ $attendingUser->user->fullname }}</td>
-							        </tr>
-						        @endforeach
-						    </tbody>
-						</table>
-					</div>
-				@else
-					<div class="alert alert-info">
-						There are no current attendings.
-					</div>
-				@endif
+				<div class="col-sm-10 col-sm-offset-1" v-if="attendingUsers.length > 0">
+					<table id="mytable" class="table table-bordred table-striped">
+					    <thead>
+					        <tr>
+					            <th>Employee</th>
+					       		<th>User</th>
+					        </tr>
+					    </thead>
+					    <tbody>
+					        <tr v-for="attendingUser in attendingUsers">
+					            <td>@{{ attendingUser.employee.fullname }}</td>
+					            <td>@{{ attendingUser.user.fullname }}</td>
+					        </tr>
+					    </tbody>
+					</table>
+				</div>
+			
+				<div class="alert alert-info" v-else>
+					There are no current attendings.
+				</div>
 			</div>
 
 			<div class="row">
 				<h2>Queue</h2>
-				@if($users->count())
-					<div class="col-sm-10 col-sm-offset-1">
-						<table id="mytable" class="table table-bordred table-striped">
-						    <thead>
-						        <tr>
-						            <th>Name</th>
-						            <th>Email</th>
-						            <th>Mobile</th>
-						        </tr>
-						    </thead>
-						    <tbody>
-						        @foreach($users as $user)
-						        	<tr>
-							            <td>{{ $user->fullname }}</td>
-							            <td>{{ $user->email }}</td>
-							            <td>{{ $user->mobile }}</td>
-							        </tr>
-						        @endforeach
-						    </tbody>
-						</table>
-					</div>
-				@else
-					<div class="alert alert-info">
-						There are no users to show.
-					</div>
-				@endif
+				<div class="col-sm-10 col-sm-offset-1" v-if="users.length > 0">
+					<table id="mytable" class="table table-bordred table-striped">
+					    <thead>
+					        <tr>
+					            <th>Name</th>
+					            <th>Email</th>
+					            <th>Mobile</th>
+					        </tr>
+					    </thead>
+					    <tbody>
+					        <tr v-for="user in users">
+					            <td>@{{ user.fullname }}</td>
+					            <td>@{{ user.email }}</td>
+					            <td>@{{ user.mobile }}</td>
+					        </tr>
+					    </tbody>
+					</table>
+				</div>
+				<div class="alert alert-info" v-else>
+					There are no users to show.
+				</div>
 			</div>
         </div>
     </div>
